e4ced224b2f8649373ed064ce33793a8a9f777c9
Aditya <Aditya.Joshi@amd.com>
2025-05-27 15:36:50 -0500

    Subiter divides mfmas , ds_reads into 4 parts

--- tensilelite/Tensile/Components/LocalRead.py
+++ tensilelite/Tensile/Components/LocalRead.py
@@ -174,7 +174,8 @@ class LocalReadMFMA(LocalRead):
         tileBlockWidth   = tP["bpeDS"]/4 if kernel["UnrollMajorLDS%s"%tc] else instruction.blockWidth
 
         vectorWidth  = kernel["VectorWidth%s"%tc]
-
+        numSubTiles = kernel["numSubTiles%s"%tc]
+        subTileIdx = kernel["SubTileIdx%s"%tc]
         MIWaveGroupShape = [ kernel["MatrixInstM"] * kernel["MatrixInstBM"] * kernel["MIWaveGroup"][0] * kernel["VectorWidthA"], \
                             kernel["MatrixInstN"] * kernel["MatrixInstBN"] * kernel["MIWaveGroup"][1] * kernel["VectorWidthB"]]
 
@@ -216,8 +217,11 @@ class LocalReadMFMA(LocalRead):
         pack     = Module("pack%s_I%s"%(tc,iui))
 
         # split Metadata when localread width > mi input
-        numSplitMetadata = max(ceil((blockWidth * 4) // tP["bpeDS"]) - 1, 0) if tP["isM"] else 0
-        valufIdx = 0
+        numSplitMetadata = max(ceil((blockWidth * 4) // (kernel["MIInputPerThread%s"%tc] * tP["bpeDS"])) - 1, 0) if tP["isM"] else 0
+        eIdxCnt = numReadsPerVector//numSubTiles
+        eIdxStart = subTileIdx * (numReadsPerVector//numSubTiles)
+        valufIdx = eIdxStart * blockWidth *numReadsPerUnroll 
+        #print("Debug: %s, eIdxStart %u, eIdxCnt %u, numReadsPerVector %u, numSubTiles %u, subTileIdx %u" % (tc, eIdxStart, eIdxCnt, numReadsPerVector, numSubTiles, subTileIdx))
         if enableLDSTr:
             numberMTilesPerWave = kernel["MIWaveTile"][tile01]
             highBits = 0
@@ -241,9 +245,9 @@ class LocalReadMFMA(LocalRead):
                 localReadCode.add(LocalReadX(dst=destVgpr, src=vgpr("LocalReadAddr%s"%tc), ds=ds, comment=comment))
         else:
             for vIdx in range(0, numVectorsPerTile):
-                for eIdx in range(0, numReadsPerVector):
+                for eIdx in range(eIdxStart, (eIdxStart + eIdxCnt)):
                     valuiIdx = int(valufIdx)
-                    localReadCode = imod.add(Module("LocalRead%s Valu%u"%(tc,valuiIdx)))
+                    localReadCode = imod.add(Module("LocalRead%s Valu%u"%(tc,valuiIdx)))                 
                     if needPack or numSplitMetadata:
                         packCode = pack.add(Module("packCode"))
                     for rIdx in range(0, numReadsPerUnroll):
